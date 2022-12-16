dde2007c23753282b1b97ddd0f141679b52904e2
christian-x86 <108787839+christian-x86@users.noreply.github.com>
2022-12-16 21:05:08 +0100

    Add files via upload

---
+++ docs/sample1/js/main.js
@@ -0,0 +1,45 @@
+function includeHTML() {
+  var z, i, elmnt, file, xhttp;
+  /* Loop through a collection of all HTML elements: */
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    /*search for elements with a certain atrribute:*/
+    file = elmnt.getAttribute("w3-include-html");
+    if (file) {
+      /* Make an HTTP request using the attribute value as the file name: */
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+          /* Remove the attribute, and call this function once more: */
+          elmnt.removeAttribute("w3-include-html");
+          includeHTML();
+        }
+      }
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      /* Exit the function: */
+      return;
+    }
+  }
+}
+
+includeHTML();
+
+// sidepanel
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  // document.getElementById("overlay").style.backgroundColor = "rgba(0,0,0,0.6)";
+  document.getElementById("overlay").style.zIndex = "1";
+  document.getElementById("overlay").style.backdropFilter = "blur(1.5px)";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  // document.getElementById("overlay").style.backgroundColor = "rgba(0,0,0,0.0)";
+  document.getElementById("overlay").style.zIndex = "-1";
+  document.getElementById("overlay").style.backdropFilter = "blur(0px)";
+  
+}
